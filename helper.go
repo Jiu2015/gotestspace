641876a2de3e9114f93b3f5d4f784e4da242b478
Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>
2022-01-14 11:32:10 +0800

    Add ExecuteWithStdinStdout on testspace
    The ExecuteWithStdinStdout will contains custom stdin and stdout for
    use, if you have interactive command operations, it will be more
    applicable.
    
    Signed-off-by: Jiuyang Xie <jiuyang.xjy@alibaba-inc.com>

--- helper.go
+++ helper.go
@@ -50,20 +50,15 @@ func SimpleExecuteCommand(ctx context.Context, path string, env []string, comman
 
 // NewTestSpaceCommand will return space-command for advantage use,
 // You must get stdin, stdout and stderr before spaceCommand.Wait(), and do not miss spaceCommand.Wait()
-func NewTestSpaceCommand(ctx context.Context, path string, env []string, enableStdin bool, stdout, stderr io.Writer,
+func NewTestSpaceCommand(ctx context.Context, path string, env []string, stdin io.Reader, stdout, stderr io.Writer,
 	commandName string, args ...string) (Commander, error) {
-	var tempStdin io.Reader
 	cmd := exec.Command(commandName, args...)
 
 	if len(path) > 0 {
 		cmd.Dir = path
 	}
 
-	if enableStdin {
-		tempStdin = setStdinType
-	}
-
-	spaceCommand, err := new(ctx, cmd, tempStdin, stdout, stderr, env...)
+	spaceCommand, err := new(ctx, cmd, stdin, stdout, stderr, env...)
 	if err != nil {
 		return nil, err
 	}
